b5a5208f64662e078c8217ec5082aac32b1de2cb
Nadia Alshariah <nadiakalshariah@gmail.com>
2023-10-08 09:36:17 +0300

    Revert "the day of the competeion"
    This reverts commit d4ca4ea09c51d18f8883df1108c2b26729f6fd62.

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import views
 from authentication.auth_blueprint import auth
 from settings.settings_blueprint import settings
 from user.user_blueprint import user_bp
-from projects.projects_bp import projects_bp
 
 app.register_blueprint(auth, url_prefix="/authentication")
 app.register_blueprint(settings, url_prefix="/settings")
 app.register_blueprint(user_bp, url_prefix="/<username>")
-app.register_blueprint(projects_bp, url_prefix = "/projects")
 
 
 if __name__ == "__main__":
